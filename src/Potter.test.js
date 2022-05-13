c9693107bd0f68c8fb8010e356f5d983834f92a6
whtang906 <whtang906@gmail.com>
2022-05-13 16:41:50 +0800

    update basic test and add simple discount test

--- src/Potter.test.js
+++ src/Potter.test.js
@@ -13,7 +13,24 @@ describe('Potter', () => {
   });
 
   test('Test on basic basket', () => {
-    potter.setBasket([1, 3, 4]);
+    potter.setBasket([]);
+    expect(potter.price()).toBe(0);
+    potter.setBasket([1]);
+    expect(potter.price()).toBe(8);
+    potter.setBasket([2]);
+    expect(potter.price()).toBe(8);
+    potter.setBasket([1, 1, 1]);
     expect(potter.price()).toBe(24);
+  });
+
+  test('Test on basic basket', () => {
+    potter.setBasket([1, 2]);
+    expect(potter.price()).toBe(8 * 2 * 0.95);
+    potter.setBasket([1, 3, 5]);
+    expect(potter.price()).toBe(8 * 3 * 0.9);
+    potter.setBasket([1, 2, 3, 4]);
+    expect(potter.price()).toBe(8 * 4 * 0.8);
+    potter.setBasket([1, 2, 3, 4, 5]);
+    expect(potter.price()).toBe(8 * 5 * 0.75);
   });
 });
